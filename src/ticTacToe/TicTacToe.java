a529c727caa5a9e4e1f564653195db3032586342
bhade <bhade@DESKTOP-AE0J8AT>
2020-02-28 10:59:31 -0800

    Added some comments

--- src/ticTacToe/TicTacToe.java
+++ src/ticTacToe/TicTacToe.java
@@ -1,19 +1,22 @@
-/** TicTacToe.java
- * Brady Haden
+/**
+ * TicTacToe.java Brady Haden
  * 
- * This GUI program displays a tic-tac-toe board and
- * allows two players to try and best one another in
- * the classic game. 
- * When a player wins or there is a tie the board is
- * automatically reset for another round.
-   * 
-   */
+ * This GUI program displays a tic-tac-toe board and allows two players to try
+ * and best one another in the classic game. When a player wins or there is a
+ * tie the board is automatically reset for another round.
+ * 
+ */
 
 package ticTacToe;
 
-import javax.swing.*;
 import java.awt.GridLayout;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class TicTacToe extends JFrame
 {
@@ -22,9 +25,9 @@ public class TicTacToe extends JFrame
   private static final int HEIGHT = 350;
   private boolean xTurn = true; // is it x's turn?
   private JButton[][] buttonsArr = new JButton[3][3];
-  
-  //*************
-  
+
+  // *************
+
   public TicTacToe()
   {
     setTitle("Tic-Tac-Toe");
@@ -32,23 +35,26 @@ public class TicTacToe extends JFrame
     setLocationRelativeTo(null);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     createContents();
-    try 
-    { 
+    // matches the users system styling
+    try
+    {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-    } 
-    catch (Exception e) 
+    } catch (Exception e)
     {
       e.printStackTrace();
     }
     setVisible(true);
   }
-  
+
+  /*
+   * This creates a grid layout of buttons using the 2s array buttonsArr
+   */
   private void createContents()
-  { 
+  {
     setLayout(new GridLayout(3, 3));
-    for (int i=0; i<3; i++)
+    for (int i = 0; i < 3; i++)
     {
-      for (int j=0; j<3; j++)
+      for (int j = 0; j < 3; j++)
       {
         buttonsArr[i][j] = new JButton();
         buttonsArr[i][j].addActionListener(new Listener());
@@ -56,7 +62,7 @@ public class TicTacToe extends JFrame
       }
     }
   }
-  
+
   private class Listener implements ActionListener
   {
     public void actionPerformed(ActionEvent e)
@@ -67,11 +73,12 @@ public class TicTacToe extends JFrame
         btn.setText(xTurn ? "X" : "O");
         if (checkForWinner()) // true if current xTurn is winner
         {
-          JOptionPane.showMessageDialog(null, (xTurn ? "X" : "O") 
-              + " is the winner!");
+          JOptionPane.showMessageDialog(null,
+              (xTurn ? "X" : "O") + " is the winner!");
           resetGame();
         }
-        else if(checkBoardFull())
+        // if board is full with no winner then tie game
+        else if (checkBoardFull())
         {
           JOptionPane.showMessageDialog(null, "Its a Tie!");
           resetGame();
@@ -81,64 +88,64 @@ public class TicTacToe extends JFrame
       }
     }
   }
-  
+
   /*
-   * method to reset the game. it clears the board and sets xTurn
-   * to its default value of 'true'.
+   * method to reset the game. it clears the board and sets xTurn to its default
+   * value of 'true'.
    */
   private void resetGame()
   {
-    for (int i=0; i<3; i++)
+    for (int i = 0; i < 3; i++)
     {
-      for (int j=0; j<3; j++)
+      for (int j = 0; j < 3; j++)
       {
         buttonsArr[i][j].setText("");
       }
     }
     xTurn = true;
   }
-  
+
   /*
-   * this checks for a winner by checking if each row on the board
-   * has 3 in a row or each column nhas 3 in a row.
-   * it then checks both the major diagonal(top left to bottom right)
-   * and the minor diagonal(top right to bottom left) for 3 in a row
+   * this checks for a winner by checking if each row on the board has 3 in a
+   * row or each column nhas 3 in a row. it then checks both the major
+   * diagonal(top left to bottom right) and the minor diagonal(top right to
+   * bottom left) for 3 in a row
    * 
    * returns boolean
    */
   private boolean checkForWinner()
   {
     // check rows
-    for (int i=0; i<3; i++)
+    for (int i = 0; i < 3; i++)
     {
-      if (buttonsArr[i][0].getText().equals(xTurn ? "X" : "O") && 
-          buttonsArr[i][1].getText().equals(xTurn ? "X" : "O") && 
-          buttonsArr[i][2].getText().equals(xTurn ? "X" : "O"))
+      if (buttonsArr[i][0].getText().equals(xTurn ? "X" : "O")
+          && buttonsArr[i][1].getText().equals(xTurn ? "X" : "O")
+          && buttonsArr[i][2].getText().equals(xTurn ? "X" : "O"))
         return true;
     }
-    
+
     // check columns
-    for (int j=0; j<3; j++)
+    for (int j = 0; j < 3; j++)
     {
-      if (buttonsArr[0][j].getText().equals(xTurn ? "X" : "O") && 
-          buttonsArr[1][j].getText().equals(xTurn ? "X" : "O") && 
-          buttonsArr[2][j].getText().equals(xTurn ? "X" : "O"))
+      if (buttonsArr[0][j].getText().equals(xTurn ? "X" : "O")
+          && buttonsArr[1][j].getText().equals(xTurn ? "X" : "O")
+          && buttonsArr[2][j].getText().equals(xTurn ? "X" : "O"))
         return true;
     }
 
     // check major diagonal
-    if (buttonsArr[0][0].getText().equals(xTurn ? "X" : "O") &&
-        buttonsArr[1][1].getText().equals(xTurn ? "X" : "O") &&
-        buttonsArr[2][2].getText().equals(xTurn ? "X" : "O"))
+    if (buttonsArr[0][0].getText().equals(xTurn ? "X" : "O")
+        && buttonsArr[1][1].getText().equals(xTurn ? "X" : "O")
+        && buttonsArr[2][2].getText().equals(xTurn ? "X" : "O"))
       return true;
-    // check minor diagonal    
-    if (buttonsArr[0][2].getText().equals(xTurn ? "X" : "O") &&
-        buttonsArr[1][1].getText().equals(xTurn ? "X" : "O") &&
-        buttonsArr[2][0].getText().equals(xTurn ? "X" : "O"))
+    // check minor diagonal
+    if (buttonsArr[0][2].getText().equals(xTurn ? "X" : "O")
+        && buttonsArr[1][1].getText().equals(xTurn ? "X" : "O")
+        && buttonsArr[2][0].getText().equals(xTurn ? "X" : "O"))
       return true;
     return false;
   }
-  
+
   /*
    * this checks if the board is full (aka a tie game)
    * 
@@ -146,9 +153,9 @@ public class TicTacToe extends JFrame
    */
   private boolean checkBoardFull()
   {
-    for (int i=0; i<3; i++)
+    for (int i = 0; i < 3; i++)
     {
-      for (int j=0; j<3; j++)
+      for (int j = 0; j < 3; j++)
       {
         if (buttonsArr[i][j].getText().isEmpty())
           return false;
